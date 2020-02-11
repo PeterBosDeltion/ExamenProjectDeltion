55379af4ef70a9005d5477a55bbd6be2d852a69e
KevinOtten <kevin.otten@ziggo.nl>
2020-02-11 15:12:21 +0100

    Quik push to switch branch

---
+++ Deltion Examen Project/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private int xAxis;
+    private int yAxis;
+    public bool placingAbility;
+}
