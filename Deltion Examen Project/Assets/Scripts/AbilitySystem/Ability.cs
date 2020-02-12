70b5e33833b43d7082c35d6cf691e946ccbbad54
vanukarp <vanukarp@gmail.com>
2020-02-12 15:15:43 +0100

    Started with abilities & player char

---
+++ Deltion Examen Project/Assets/Scripts/AbilitySystem/Ability.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ability : MonoBehaviour
+{
+    private int requiredLevel;
+    public bool ultimate;
+    protected bool onCooldown;
+    public float cooldownTime;
+    public float duration;
+    protected float currentUltCharge;
+    public float maxDeployableDistance = 15;
+    public enum DeployType
+    {
+        Instant,
+        Deployed,
+        LaserTarget
+    }
+    public DeployType myDeployType;
+
+    public void UseAbility()
+    {
+        switch (myDeployType)
+        {
+            case DeployType.Instant:
+                AbilityMechanic();
+                break;
+            case DeployType.Deployed:
+                RaycastHit hit;
+                if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+                {
+                    Vector3 mPos = hit.point;
+                    if (Vector3.Distance(transform.position, mPos) <= maxDeployableDistance)
+                    {
+                        AbilityMechanic();
+                    }
+                }
+                break;
+            case DeployType.LaserTarget:
+                break;
+        }
+    }
+
+    protected abstract void AbilityMechanic();
+}
