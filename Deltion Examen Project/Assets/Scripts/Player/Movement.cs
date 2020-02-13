1c2af4d9721c94be6d2da9a427a8b4bdf69e406a
KevinOtten <kevin.otten@ziggo.nl>
2020-02-13 17:09:15 +0100

    Functioning movement

--- Deltion Examen Project/Assets/Scripts/Player/Movement.cs
+++ Deltion Examen Project/Assets/Scripts/Player/Movement.cs
@@ -7,19 +7,29 @@ public class Movement : MonoBehaviour
     public float movementSpeed;
     public float rotationSpeed;
 
-    public Vector3 forward;
+    private Vector3 forward;
+    private Vector3 Right;
 
     void Awake()
     {
         forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        Right = Quaternion.Euler(0, 90, 0) * forward; 
     }
 
     public void Move(float xAxis, float yAxis)
     {
-        Vector3 toMove = new Vector3(xAxis, 0, yAxis);
-        toMove *= movementSpeed;
-        toMove *= Time.deltaTime;
-        transform.Translate(toMove);
+        //Vector3 toMove = new Vector3(xAxis, 0, yAxis);
+        //toMove *= movementSpeed;
+        //toMove *= Time.deltaTime;
+        //transform.Translate(toMove);
+        Vector3 direction = new Vector3(xAxis, 0, yAxis);
+        Vector3 rightMovement = Right * movementSpeed * Time.deltaTime * xAxis;
+        Vector3 upMovement = forward * movementSpeed * Time.deltaTime * yAxis;
+
+        transform.position += rightMovement;
+        transform.position += upMovement;
     }
 
     public void Rotate(float xAxis, float zAxis)
