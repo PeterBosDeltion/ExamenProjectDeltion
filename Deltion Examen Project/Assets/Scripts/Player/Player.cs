aa0ba534599aa0cf3592ed836509542d641a8370
vanukarp <vanukarp@gmail.com>
2020-02-14 15:10:29 +0100

    Working on UI

--- Deltion Examen Project/Assets/Scripts/Player/Player.cs
+++ Deltion Examen Project/Assets/Scripts/Player/Player.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public List<Ability> abilities = new List<Ability>();
+    public Weapon primary;
 
-    public float hp = 1000;
+    public float maxHp = 1000;
+    public float hp;
     public float tempHp;
     // Update is called once per frame
 
@@ -19,19 +21,23 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            abilities[0].UseAbility();
+            if (!abilities[0].onCooldown)
+                    abilities[0].UseAbility();
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            abilities[1].UseAbility();
+            if (!abilities[1].onCooldown)
+                    abilities[1].UseAbility();
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            abilities[2].UseAbility();
+            if (!abilities[2].onCooldown)
+                    abilities[2].UseAbility();
         }
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-            abilities[3].UseAbility();
+            if (!abilities[3].onCooldown)
+                    abilities[3].UseAbility();
         }
         //[PH]
     }
@@ -42,6 +48,8 @@ public class Player : MonoBehaviour
         //{
         //    InputManager.Instance.abilityEvent += abilities[i].UseAbility;
         //}
+
+        hp = maxHp;
     }
     public void TakeDamage(float amount)
     {
---
+++ Deltion Examen Project/Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class PlayerUI : MonoBehaviour
+{
+    public Player myPlayer;
+
+    public TextMeshProUGUI ultCharge;
+    public Image weaponImage;
+
+    public Image abilityOneCDImg;
+    public Image abilityTwoCDImg;
+    public Image abilityThreeCDImg;
+    public Image abilityFourCDImg;
+
+    public Image healthBar;
+
+    //private bool waiting;
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        InputManager.Instance.abilityEvent += AbilityUsed;
+    }
+
+    public void AbilityUsed(float f)
+    {
+            switch (f)
+            {
+                case 0:
+                    if (abilityOneCDImg.fillAmount == 0)
+                        abilityOneCDImg.fillAmount = 1;
+                    break;
+                case 1:
+                    if (abilityTwoCDImg.fillAmount == 0)
+                        abilityTwoCDImg.fillAmount = 1;
+                    break;
+                case 2:
+                    if (abilityThreeCDImg.fillAmount == 0)
+                        abilityThreeCDImg.fillAmount = 1;
+                    break;
+                case 3:
+                    if (abilityFourCDImg.fillAmount == 0)
+                        abilityFourCDImg.fillAmount = 1;
+                    break;
+                default:
+                    break;
+            }
+    }
+
+
+    private void Update()
+    {
+        if (myPlayer.abilities[0].onCooldown)
+        {
+            abilityOneCDImg.fillAmount -= Time.deltaTime / myPlayer.abilities[0].cooldownTime;
+        }
+        if (myPlayer.abilities[1].onCooldown)
+        {
+            abilityTwoCDImg.fillAmount -= Time.deltaTime / myPlayer.abilities[1].cooldownTime;
+        }
+        if (myPlayer.abilities[2].onCooldown)
+        {
+            abilityThreeCDImg.fillAmount -= Time.deltaTime / myPlayer.abilities[2].cooldownTime;
+        }
+        if (myPlayer.abilities[3].onCooldown)
+        {
+            abilityFourCDImg.fillAmount -= Time.deltaTime / myPlayer.abilities[3].cooldownTime;
+        }
+
+        weaponImage.fillAmount =  myPlayer.primary.magazineAmmo / myPlayer.primary.totalAmmo;
+        healthBar.fillAmount = myPlayer.hp / myPlayer.maxHp;
+    }
+    //private IEnumerator AbilityCooldown(Image abilityCDImg, float seconds)
+    //{
+    //    waiting = true;
+    //    abilityCDImg.fillAmount = 1;
+
+    //    //for (float t = 0; t <= seconds; t += Time.deltaTime)
+    //    //{
+    //    //        Debug.Log(t);
+    //    //        abilityCDImg.fillAmount = lerped;
+    //    //}
+    //    for (float f = 0; f <= seconds; f += Time.deltaTime)
+    //    {
+    //    }
+    //    yield return new WaitForSeconds(seconds);
+    //    abilityCDImg.fillAmount = 0;
+    //    waiting = false;
+    //}
+
+}
