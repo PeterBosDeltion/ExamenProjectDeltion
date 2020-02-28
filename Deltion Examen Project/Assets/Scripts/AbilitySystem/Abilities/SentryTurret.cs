c10a5476e7fcd1527363019685c1f1d90b968815
vanukarp <vanukarp@gmail.com>
2020-02-28 10:46:14 +0100

    WIP

---
+++ Deltion Examen Project/Assets/Scripts/AbilitySystem/Abilities/SentryTurret.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SentryTurret : MonoBehaviour
+{
+    public GameObject rotatingBody;
+    private float myRange;
+    private float myDamage;
+
+   public void Initialize(float range, float damage)
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
---
+++ Deltion Examen Project/Assets/Scripts/AbilitySystem/Abilities/SentryTurretAbility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SentryTurretAbility : Ability
+{
+    public GameObject turretPrefab;
+
+    protected override void AbilityMechanic()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override IEnumerator AfterDuration()
+    {
+        throw new System.NotImplementedException();
+    }
+}
