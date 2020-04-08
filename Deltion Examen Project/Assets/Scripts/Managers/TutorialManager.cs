08e33bd604f59ef4d0eef0e69338a4a0e43bd892
vanukarp <vanukarp@gmail.com>
2020-04-08 16:29:40 +0200

    WIP tutorial

---
+++ Deltion Examen Project/Assets/Scripts/Managers/TutorialManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TutorialManager : MonoBehaviour
+{
+    public static TutorialManager instance;
+    public delegate void TutorialStepOne();
+    public static TutorialStepOne stepOneDelegate;
+    public List<string> tutorialTexts = new List<string>();
+    public TextMeshProUGUI tutorialText;
+
+    private GameObject currentIndicator;
+    public GameObject mainUIIndicator;
+    public GameObject weaponIndicator;
+    public GameObject healthIndicator;
+    public GameObject abilitiesIndicator;
+    public GameObject ultimateIndicator;
+
+    public GameObject tutorialEnemy;
+    public GameObject tutorialInteractable;
+
+    public PlayerController player;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        AddEmptyDelegates();
+        stepOneDelegate += StepOne;
+
+        stepOneDelegate.Invoke();
+    }
+
+    private void AddEmptyDelegates()
+    {
+        stepOneDelegate += Empty;
+    }
+
+    private void Empty()
+    {
+
+    }
+
+    private void StepOne() //WASD movement
+    {
+        tutorialText.text = tutorialTexts[0];
+    }
+}
