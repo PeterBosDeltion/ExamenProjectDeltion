89bee37c6e4316626d4fdc56024a5834b100bf68
vanukarp <vanukarp@gmail.com>
2020-05-20 15:05:08 +0200

    Quick minimap icon update

--- Deltion Examen Project/Assets/Scripts/MinimapIcon.cs
+++ Deltion Examen Project/Assets/Scripts/MinimapIcon.cs
@@ -33,7 +33,7 @@ public class MinimapIcon : MonoBehaviour
         switch (myIconType)
         {
             case IconType.Player:
-                transform.localScale = new Vector3(0.3F, 0.3F, 0.3F);
+                transform.localScale = new Vector3(0.4F, 0.4F, 0.4F);
                 icon.color = playerColor;
                 if (transform.parent.GetComponentInParent<PlayerController>().playerNumber == 0)
                 {
@@ -42,15 +42,15 @@ public class MinimapIcon : MonoBehaviour
                 }
                 break;
             case IconType.Enemy:
-                transform.localScale = new Vector3(0.3F, 0.3F, 0.3F);
+                transform.localScale = new Vector3(0.4F, 0.4F, 0.4F);
                 icon.color = EnemyColor;
                 break;
             case IconType.Objective:
-                transform.localScale = new Vector3(1.0F, 1.0F, 1.0F);
+                transform.localScale = new Vector3(10F, 10F, 10F);
                 icon.color = ObjectiveColor;
                 break;
             case IconType.Interactable:
-                transform.localScale = new Vector3(0.5F, 0.5F, 0.5F);
+                transform.localScale = new Vector3(5, 5, 5);
                 icon.color = interactableColor;
                 break;
             case IconType.Deployable:
