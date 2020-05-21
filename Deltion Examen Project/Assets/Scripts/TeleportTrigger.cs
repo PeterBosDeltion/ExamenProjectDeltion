dca8fa49172763bc914b3b30a115ca27cfa2b6cd
vanukarp <vanukarp@gmail.com>
2020-05-21 13:08:43 +0200

    Changes

--- Deltion Examen Project/Assets/Scripts/TeleportTrigger.cs
+++ Deltion Examen Project/Assets/Scripts/TeleportTrigger.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class TeleportTrigger : MonoBehaviour
 {
     public Transform targetPosition;
-
+    public GameObject toActivate;
     public void OnTriggerEnter(Collider other)
     {
         other.transform.position = targetPosition.position;
+        toActivate.SetActive(true);
     }
 }
